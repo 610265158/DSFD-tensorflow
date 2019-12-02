5aac172d5b8588bb37b813eda28edbb321465b12
lz <2120140200@mail.nankai.edu.cn>
2019-12-02 23:32:28 +0800

    add fpn

--- tmp.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-a=np.fromfile('/home/lz/mace/build/cmake-build/host/mace/tools/mace.out_tower_0_outputs_0',dtype=np.float32)
-a=np.reshape(a,[1,4200,5])
-print(a)
-print(a.shape)
-
